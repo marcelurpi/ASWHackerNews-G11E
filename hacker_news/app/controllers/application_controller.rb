890d6ef3d140c06b3b5a80547e8d9d116e18bf58
Carlos Rasche <carlos.rasche@estudiantat.upc.edu>
2021-10-19 16:29:51 +0000

    Afegit model post

--- hacker_news/app/controllers/application_controller.rb
+++ hacker_news/app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def hello
-    render html: "<h1>It works WASLAB05!</h1>".html_safe
+    render html: "<h1>It works HackerNews!</h1>".html_safe
   end
 end
