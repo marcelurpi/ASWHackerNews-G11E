cd2194a2352822431392c1de15caccf59e38ebc8
tatiana.ingrid.meyer <tatiana.ingrid.meyer@estudiantat.upc.edu>
2021-10-19 19:31:23 +0000

    submit correcto, fallo del authenticity token

--- hacker_news/db/schema.rb
+++ hacker_news/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211019162801) do
+ActiveRecord::Schema.define(version: 20211019190212) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "author"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20211019162801) do
     t.integer  "numcomments"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "title"
   end
 
 end
