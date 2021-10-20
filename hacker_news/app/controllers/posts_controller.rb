d013d4a71cabc4c3048d5b8f5fbe0fd0fe2e65a0
tatiana.ingrid.meyer <tatiana.ingrid.meyer@estudiantat.upc.edu>
2021-10-20 08:01:26 +0000

    Submit finalizado!

--- hacker_news/app/controllers/posts_controller.rb
+++ hacker_news/app/controllers/posts_controller.rb
@@ -64,6 +64,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:author, :content, :points, :numcomments, :created_at, :updated_at, :title)
+      params.permit(:created_at, :updated_at,  :title, :content)
     end
 end
