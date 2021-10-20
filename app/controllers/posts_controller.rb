d3a9ce7030585c4d8f2ab2cc8c8a2c593c146522
tatiana.ingrid.meyer <tatiana.ingrid.meyer@estudiantat.upc.edu>
2021-10-20 08:05:00 +0000

    Cambios menores: al crear va directamente a index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,8 +25,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        format.html { redirect_to posts_url, notice: "Post was successfully created." }
+        format.json { head :no_content }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
