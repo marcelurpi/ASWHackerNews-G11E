f1ab84e6b5a9f1cbceb9e52538612514bab178e1
Marcel Urpi <marcel.urpi@estudiantat.upc.edu>
2021-10-20 09:15:17 +0000

    Updated Post model

--- db/migrate/20211019162801_create_posts.rb
+++ db/migrate/20211019162801_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
       t.string :author
-      t.text :content
+      t.string :title
+      t.string :url
       t.integer :points
       t.integer :numcomments
 
