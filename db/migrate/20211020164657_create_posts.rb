2d4acb60f77c7cb3ce8e68d96fc151256dde43af
Carlos Rasche <carlos.rasche@estudiantat.upc.edu>
2021-10-21 13:40:19 +0000

    Logo pagina principal

---
+++ db/migrate/20211020164657_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :url
+      t.string :content
+      t.string :author
+      t.integer :numcomments, default: 0
+      t.integer :points, default: 0
+
+      t.timestamps
+    end
+  end
+end
