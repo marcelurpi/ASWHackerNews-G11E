760fd9a73b37996a810d86baf57654a643ebc3be
tatiana.ingrid.meyer <tatiana.ingrid.meyer@estudiantat.upc.edu>
2021-10-21 14:30:05 +0000

    Migration correct

--- hacker_news/db/migrate/20211020164657_create_posts.rb
+++ hacker_news/db/migrate/20211020164657_create_posts.rb
@@ -5,15 +5,8 @@ class CreatePosts < ActiveRecord::Migration[5.0]
       t.string :url
       t.string :content
       t.string :author
-<<<<<<< HEAD:hacker_news/db/migrate/20211019162801_create_posts.rb
-      t.string :title
-      t.string :url
-      t.integer :points
-      t.integer :numcomments
-=======
       t.integer :numcomments, default: 0
       t.integer :points, default: 0
->>>>>>> new_page:hacker_news/db/migrate/20211020164657_create_posts.rb
 
       t.timestamps
     end
