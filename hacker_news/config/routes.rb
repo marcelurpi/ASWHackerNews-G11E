b880c44646ac5ca8f8c2f74732c9b2848cdb8e9f
Marcel Urpi <marcel.urpi@estudiantat.upc.edu>
2021-10-26 22:50:53 +0000

    Added Google signup/login. Added User model

--- hacker_news/config/routes.rb
+++ hacker_news/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+    get 'login', to: 'login#new'
+    get 'login/create', to: 'login#create', as: :create_login
+    get 'login/delete', to: 'login#delete', as: :delete_login
     get 'posts/newest', to: 'posts#newest'
     resources :submissions
     resources :posts
