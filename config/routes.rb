614741dbba26b3b2c0e0958cdf900abc65dbb9e2
Carlos Rasche <carlos.rasche@estudiantat.upc.edu>
2021-10-28 13:51:10 +0000

    Like i dislike implementats

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'posts/newest', to: 'posts#newest'
   resources :submissions
-  resources :posts
+  resources :posts do
+    put 'like', on: :member and
+    put 'unlike', on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   
