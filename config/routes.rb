ca1209736e3b34199f640daaf4a164a03022ce6c
carlosexojo <carlos.exojo@estudiantat.upc.edu>
2021-10-31 10:41:44 +0000

    Added ask brach

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'ask/index'
     get 'login', to: 'login#new'
     get 'login/create', to: 'login#create', as: :create_login
     get 'login/delete', to: 'login#delete', as: :delete_login
     get 'posts/newest', to: 'posts#newest'
+    get 'posts/ask', to: 'posts#ask'
     resources :submissions
     resources :posts
     root 'posts#index'
