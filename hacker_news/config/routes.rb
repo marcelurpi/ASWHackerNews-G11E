4ab01a336610a1ecc82bc434a27f43abc96aec24
Marcel Urpi <marcel.urpi@estudiantat.upc.edu>
2021-11-11 12:45:05 +0000

    Added profile viewing

--- hacker_news/config/routes.rb
+++ hacker_news/config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-
-  get 'login', to: 'login#new'
-  get 'login/create', to: 'login#create', as: :create_login
-  get 'login/delete', to: 'login#delete', as: :delete_login
-  get 'posts/newest', to: 'posts#newest'
-  resources :submissions
-  resources :posts do
-    put 'like', on: :member and
-    put 'unlike', on: :member and
-    put 'comment', on: :member
-  end
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
-
+    get 'login', to: 'login#new'
+    get 'login/profile', to: 'login#profile', as: :profile_login
+    post 'login/profile', to: 'login#update', as: :update_login
+    get 'login/create', to: 'login#create', as: :create_login
+    get 'login/delete', to: 'login#delete', as: :delete_login
+    get 'posts/newest', to: 'posts#newest'
+    resources :submissions
+    resources :posts
+    root 'posts#index'
 end
--- hacker_news/db/schema.rb
+++ hacker_news/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_182916) do
+ActiveRecord::Schema.define(version: 2021_11_11_121529) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "id_comment"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_10_28_182916) do
     t.string "google_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "about"
   end
 
 end
