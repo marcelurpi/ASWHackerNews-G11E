e980b9f5f8e476bf56655b9b0d4782a759820ad7
Carlos Rasche <carlos.rasche@estudiantat.upc.edu>
2021-11-13 01:14:46 +0000

    Afegida funcionalitat like amb usuari NO PROVADA

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_121529) do
+ActiveRecord::Schema.define(version: 2021_11_12_230802) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "comment_id"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_11_11_121529) do
     t.integer "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
