6bf406ce6c11afd402421d9b5cbea953805aee1d
clara0201 <clara.estruch@estudiantat.upc.edu>
2021-11-13 01:52:11 +0000

    Implementat show de comentari per poder fer reply

--- hacker_news/config/routes.rb
+++ hacker_news/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'ask/index'
+    get 'ask/index'
     get 'login', to: 'login#new'
     get 'login/profile', to: 'login#profile', as: :profile_login
     post 'login/profile', to: 'login#update', as: :update_login
@@ -7,16 +7,17 @@ Rails.application.routes.draw do
     get 'login/delete', to: 'login#delete', as: :delete_login
     get 'posts/newest', to: 'posts#newest'
     get 'posts/ask', to: 'posts#ask'
+    
     resources :submissions
     resources :posts do
       put 'like', on: :member and
       put 'unlike', on: :member and
-      post 'comment', on: :member
+      post 'comment', on: :member and
       resources :comments
     end
     resources :comments do
-      resources :comments
-      post 'comment', on: :member
+      post 'comment', on: :member and
+      resources :comments 
     end
     root 'posts#index'
 end
