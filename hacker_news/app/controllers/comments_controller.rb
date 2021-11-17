a897d04de82b77d0af4311b2f6825625dc75160d
clara0201 <86661396+clara0201@users.noreply.github.com>
2021-11-17 16:44:49 +0100

    Revert "Millores show"

--- hacker_news/app/controllers/comments_controller.rb
+++ hacker_news/app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_action :find_commentable, :set_comment, only: [:show, :update, :destroy, :comment]
+  before_action :set_comment, only: [:show, :update, :destroy]
 
   # GET /comments
   # GET /comments.json
@@ -20,31 +20,14 @@ class CommentsController < ApplicationController
   # GET /comments/1/edit
   def edit
   end
-  
-  # PUT /posts/1/comment
-  def comment
-    if cookies.signed[:user_id].nil?
-      redirect_to(login_path)
-      
-    else
-    
-      @child = @commentable.comments.create(content: params[:content], user_id: params[:user_id])
-    
-      redirect_to (@commentable)
-    end
-  end
-  
-  
 
   # POST /comments
   # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
-    
-    #@comment = Comment.new(comment_params)
 
     respond_to do |format|
-      if @comment.save
+      if @comment.saver
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
@@ -64,8 +47,7 @@ class CommentsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
- 
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -75,16 +57,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content, :commentable_id, :commentable_type, :user_id)
-    end
-    
-    def find_commentable
-      if params[:comment_id]
-        @commentable = Comment.find_by_id(params[:comment_id])
-        logger.debug "parent is a comment"
-      else params[:post_id]
-        @commentable = Post.find_by_id(params[:post_id])
-        logger.debug "parent is a post"
-      end
+      params.require(:comment).permit(:content, :created_at, :updated_at, :comment_id, :user_id)
     end
 end
--- hacker_news/app/models/post.rb
+++ hacker_news/app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ApplicationRecord
-  has_many :comments, as: :commentable
-  has_many :likes, dependent: :destroy
+  has_many :comments
 end
