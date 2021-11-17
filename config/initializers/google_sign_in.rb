e31fc6758b20afc24010b7aa18d5812a594f0456
Marcel Urpi <marcel.urpi@estudiantat.upc.edu>
2021-11-17 23:40:42 +0000

    fix

---
+++ config/initializers/google_sign_in.rb
@@ -0,0 +1,4 @@
+Rails.application.configure do
+  config.google_sign_in.client_id     = Rails.application.credentials.google_sign_in_client_id
+  config.google_sign_in.client_secret = Rails.application.credentials.google_sign_in_client_secret
+end
---
+++ db/schema.rb
@@ -0,0 +1,49 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_11_11_121529) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "id_comment"
+    t.string "content"
+    t.integer "points", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.integer "user_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.string "content"
+    t.string "author"
+    t.integer "numcomments", default: 0
+    t.integer "points", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "id_post"
+    t.index ["id_post"], name: "index_posts_on_id_post"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "google_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "about"
+  end
+
+end
