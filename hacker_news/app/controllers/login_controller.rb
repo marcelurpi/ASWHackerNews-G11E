83ecdd406a601cc5262287a391434f849e8c1cbc
Carlos Rasche <carlos.rasche@estudiantat.upc.edu>
2021-12-21 23:13:46 +0000

    added check for self profile

--- hacker_news/app/controllers/login_controller.rb
+++ hacker_news/app/controllers/login_controller.rb
@@ -14,6 +14,12 @@ class LoginController < ApplicationController
     @usuaris = User.all
     if !params[:usuari_id].nil? && params[:usuari_id]
       p 'primer if'
+      if params[:usuari_id] == "-1"
+        key   = ActiveSupport::KeyGenerator.new(ENV['SECRET_KEY']).generate_key(ENV['ENCRYPTION_SALT'], ActiveSupport::MessageEncryptor.key_len)
+        crypt = ActiveSupport::MessageEncryptor.new(key)
+        author_id = crypt.decrypt_and_verify(params['X-API-KEY'])
+        @usuaris = User.where(id: author_id)
+      else
         @usuaris = User.where(id: params[:usuari_id] )
         if @usuaris.nil?  #si el usuario es null
           respond_to do |format|
@@ -22,6 +28,7 @@ class LoginController < ApplicationController
           end
           return
         end
+      end
     end
     respond_to do |format|
           format.html
